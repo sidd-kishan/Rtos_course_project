3781ab8930d56c2a9e010ebf0aed2b3f75963908
Aayush Deo <aayush.deo1994@gmail.com>
2023-07-17 17:46:29 +0530

    Update FreeRTOSConfig.h
    compilation error

--- FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/FreeRTOSConfig.h
+++ FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/FreeRTOSConfig.h
@@ -61,7 +61,8 @@
 #define configUSE_QUEUE_SETS                    1
 #define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
-#define configENABLE_BACKWARD_COMPATIBILITY     0
+// todo need this for lwip FreeRTOS sys_arch to compile
+#define configENABLE_BACKWARD_COMPATIBILITY     1
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
 /* System */
